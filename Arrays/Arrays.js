a9491c5cde99a378b8635f1df8ca4d537fae2c1c
Shabhaizafar <zafarshabhai03.com>
2024-07-28 16:39:06 +0530

    Array

---
+++ Arrays/Arrays.js
@@ -0,0 +1,95 @@
+// => Arrays in JavaScript :-
+
+//    - Intro to arrays.
+// - Collection of Multiple Data with same or different Datatype.
+
+// var Arr = [];
+// console.log(Arr);
+// console.log(typeof Arr);
+
+
+// var Arr2 = [1,2,3,4,5];
+// var Arr2 = ["Zafar","Royal","Techno"];
+// var Arr2 = [1,2,3,"Zafar","Royal",true];
+
+// console.log(Arr2);
+// console.log(typeof Arr2);
+
+/////////////////////////
+//index and length : 
+
+
+// var Arr = ["Zafar","Royal",1000,true,"Extra",1234,"ABC","Javascript"];
+// //index       0       1      2    3     4
+
+// //How to Print Array 
+// console.log(Arr);
+
+// //How to Print Single Array's Value
+// console.log(Arr[3]);
+
+
+// //How to Check Array length 
+// // property : length
+// console.log(Arr.length);
+
+// //How to Print Last Value of Array
+// console.log(Arr[Arr.length-1]); //Arr[5-1]  = Arr[4]
+
+
+
+//////////////////////////////////////////
+// Basic Method of Array :
+//    - Push pop shift unshift.
+
+var Arr = [11,12,13,14,15];
+console.log(Arr);
+
+
+// 1) push()  : add new Value  at the last index
+// retun new length
+
+// Arr.push(100); // for single Value
+// var ans = Arr.push(100,200,300); // for Multiple Value
+// console.log(Arr);
+
+// 2) pop() : remove value from the Last Index 
+ //remove only one value at a single time
+// return a deleted value
+
+// Arr.pop(); 
+// var ans =  Arr.pop(); 
+// console.log(Arr);
+// console.log(ans);
+
+
+// 3) shift() : remove value from the First Index
+// return a deleted value
+// Arr.shift();
+// var ans = Arr.shift();
+// console.log(Arr);
+
+// 4) unshift() : add new Value  at the Starting index
+// retun new length
+
+// Arr.unshift(100);
+// var ans = Arr.unshift(100,200,300);
+// console.log(Arr);
+// console.log(ans);
+///////////////////////////////
+// 1) Create an Array with diffrent Data type and print All values.
+// 2) Add new Element last postion using push and print an Array.
+// 3) Add new Element starting position using unshft and print an array.
+// 4) remove 2 values starting and ending index
+
+////////////////
+
+
+//    - Primitive vs reference data types.
+//    - Clone array & spread operator.
+//    - For loop.
+//    - use const for creating arrays.
+//    - While loop in array.
+//    - For of loop.
+//    - For in loop.
+//    - Array destructuring.
