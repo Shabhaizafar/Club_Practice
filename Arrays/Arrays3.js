7c7f52e76595fd5edcfbb54035f310d925619d54
Shabhaizafar <zafarshabhai03.com>
2024-07-30 18:54:05 +0530

    Object

---
+++ Arrays/Arrays3.js
@@ -0,0 +1,46 @@
+//    - use const for creating arrays.
+// const Arr = [11,1,2,1,3,14];
+// console.log(Arr);
+
+// Arr.push(1000);
+// console.log(Arr);
+
+// Arr.pop();
+
+// // Arr = [];
+// console.log(Arr);
+
+
+//    - Array destructuring. : 
+
+// const Arr = [11,1,2,1,3,14,15,16,17,18];
+
+// var a = Arr[0];
+// var b = Arr[1];
+// var b = Arr[2];
+
+
+
+// console.log(a);
+
+
+// var [v1,v2,,,v3,...newArr] = Arr;
+
+// console.log(v1);
+// console.log(v2);
+// console.log(v3);
+// console.log(newArr);
+
+
+
+///////////////////////////////////////
+// Bonus Challenge
+// Nested Array Destructuring:
+
+// Given the nested array let nestedArray = [[1, 2], [3, 4], [5, 6]];, use destructuring to extract the first element of each sub-array.
+// -------------------------------------------
+// Using All Loops:
+
+// Write functions that use each type of loop (for, for...of, forEach,for in) to calculate the sum of all elements in an array of numbers.
+
+
---
+++ Object/obj1.js
@@ -0,0 +1,96 @@
+// - Intro to objects.
+// object is a Collection of Multiple Properties and Methods.
+// - key and value  pair           property 
+// - Method
+
+
+//How to Create Simple Object : 
+// -using Object Literal  : {}
+// var obj = {};
+// console.log(obj);
+// console.log(typeof obj);
+
+
+/Soon......./
+//  using Object Constructor
+//  using Object.create()
+//  using Object.assign()
+//  using prototype base Function Constructor 
+
+/////////////////////////
+// var obj = {
+//     //keys +  value     = property
+//     fname : "Raj",
+//     lname : "Patel",
+//     Age : 12
+// };
+
+//How  to Access Full Object
+// console.log(obj);
+
+
+//How to Access Property Value : 
+// 1) Dot Notation  :    objectname.propertyname
+// console.log(obj.fname);
+// 2) Bracket Notation   objectname["propertyname"]
+// console.log(obj['lname']);
+// console.log(obj["lname"]);
+// console.log(obj[`lname`]);
+
+
+//How to Modify/Change Property Value : 
+// 1) Dot Notation  :    objectname.propertyname
+// obj.fname = "Ajay";
+// console.log(obj);
+// // 2) Bracket Notation   objectname["propertyname"]
+// obj["Age"] = 30;
+// console.log(obj);
+
+
+//How to Add new Property : 
+// 1) Dot Notation  :    objectname.propertyname
+// obj.Gender = "Male";
+// console.log(obj);
+
+// 2) Bracket Notation   objectname["propertyname"]
+// obj['Hobby'] = "sleeping";
+// console.log(obj);
+
+
+//How to Delete Property : 
+// 1) Dot Notation  :    objectname.propertyname
+// delete obj.fname;
+// console.log(obj);
+
+// 2) Bracket Notation   objectname["propertyname"]
+// delete obj['Age'];
+// console.log(obj);
+
+
+//Not Possible 
+// delete obj;
+// console.log(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+// - Dot vs Bracket Notation.
+// - Iterate objects.
+// - Computed properties.
+// - Spread operator in objects.
+// - Object Destructuring.
+// - Objects inside Array.
+// - Nested Destructuring.
+
+
+
+
+// high level ,programing language ,interpreted , Object based ,prototype based programing Language
