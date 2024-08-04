81bce8072c10a61356541e3dd9ef9f8a257763ae
Shabhaizafar <zafarshabhai03.com>
2024-08-04 16:49:15 +0530

    Functions

---
+++ Function/function1.js
@@ -0,0 +1,37 @@
+// Functions in JavaScript :-
+
+// 1) Function Declaration.
+    // - without argument and without return type 
+    // - with argument and without return type 
+    // - without argument and with return type 
+    // - with argument and with return type 
+
+// 2) Expression Function.
+    // - without argument and without return type 
+    // - with argument and without return type 
+    // - without argument and with return type 
+    // - with argument and with return type 
+
+// 3) Arrow Function. 
+    // - without argument and without return type 
+    // - with argument and without return type 
+    // - without argument and with return type 
+    // - with argument and with return type 
+
+// 4) Anonymous Function.
+
+// 5) IIFE Function. 
+    // - without argument and without return type 
+    // - with argument and without return type 
+    // - without argument and with return type 
+    // - with argument and with return type 
+
+// 6) Generator Function. 
+
+// 7) recursion. 
+
+// 8) Callback Function. 
+
+// 9) Higher Order Function. 
+
+// 10) Getter and Setter 
