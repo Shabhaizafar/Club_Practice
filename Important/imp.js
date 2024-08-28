24cc49bc6c69dea94303eae74c667c85ef3a4294
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-08-28 16:40:39 +0530

    Set

--- Important/imp.js
+++ Important/imp.js
@@ -57,16 +57,16 @@
 
 //////////////////////////////////////
 //    - Object.assign.
-var obj = {
-    fname : "Raj",
-    lname : "Shah",
-    Age : 23
-};
+// var obj = {
+//     fname : "Raj",
+//     lname : "Shah",
+//     Age : 23
+// };
 
-var obj2 = {
-    gender : "Male"
-}
-console.log(obj);
+// var obj2 = {
+//     gender : "Male"
+// }
+// console.log(obj);
 
 //use for create a Shallow copy Object
 // var newOne = Object.assign(obj);
@@ -86,16 +86,98 @@ console.log(obj);
 
 
 
-var newOne = Object.assign({},obj,obj2);
-console.log(newOne);
+// var newOne = Object.assign({},obj,obj2);
+// console.log(newOne);
 
-console.log(obj);
-console.log(obj2);
-
-
-
-
+// console.log(obj);
+// console.log(obj2);
 
 ////////////////////////////////
 //    - Sets.
+//Sets in JavaScript are collections of unique values, meaning no duplicates are allowed.
+//  They provide efficient ways to store and manage distinct elements.
+//  Sets support operations like adding, deleting, and checking the presence of items, enhancing performance for tasks requiring uniqueness.
+
+var Arr = [11,12,13,14,15,11,12,13];
+console.log(Arr);
+// console.log(Array.isArray(Arr));//true
+// console.log(Array.isArray("Zafar"));//false
+
+
+
+// How to Create a Empty Set : 
+// var mySet = new Set();
+// console.log(mySet);
+
+// console.log(typeof mySet); //object
+
+// var mySet = new Set([11,12,13]);
+
+var mySet = new Set(Arr);
+console.log(mySet);
+
+// How to Add a New Value : 
+// mySet.add(100);
+// mySet.add(100,300); //Only Added single Value
+// mySet.add(11,300);
+
+
+// How to Empty Full set.
+// mySet.clear();
+
+
+//How to Delete Specific Value
+// mySet.delete(13);
+
+// mySet.delete(13,14);
+// console.log(mySet);
+
+
+
+
+// console.log(mySet.entries());
+// console.log(mySet.keys());
+// console.log(mySet.values());
+
+
+// Itrate Set Values :
+// mySet.forEach(
+//     (a)=>{
+//         console.log(a*2);
+//     }
+// );
+
+
+// console.log(mySet.has(111));
+
+// console.log(mySet.size);
+///////////////////////////
+// Union of Sets:  
+//  {1,2,3,4,5} : {2,3,4,5,6,7,8}
+//  {1,2,3,4,5,6,7,8}
+
+// Create two Sets and find their union. Print the resulting Set.
+
+///////////////////////////////////////
+
+// Intersection of Sets:
+//  {1,2,3,4,5} : {2,3,4,5,6,7,8}
+//  {2,3,4,5}
+// Create two Sets and find their intersection. Print the resulting Set.
+/////////////////////////////////////
+//-----------------------------------
+// Difference of Sets:
+//  {1,2,3,4,5} : {2,3,4,5,6,7,8}
+// {1}
+
+// Create two Sets and find their difference (elements in Set1 that are not in Set2). Print the resulting Set.
+/////////////////////////////////////////
+// Convert Set to Array:
+
+// Convert a Set back to an array. Print the array.
+// {1,2,3} 
+// [1,2,3]
+
+
+///////////////////////////////////////
 //    - Maps.
